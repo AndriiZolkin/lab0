c6aca29c359b6f9bdc4573a3c88571cae617c9c5
Андрей <@DESKTOP-QP4ME5Q>
2023-09-17 23:15:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+internal class Program
+{
+    static double SubtractionOfNumbers(double number1, double number2)
+    {
+        return number1 - number2;
+    }
+    static void Main()
+    {
+        Random random = new Random();
+        double number1 = random.NextDouble() * 100 - 50 ;
+        double number2 = random.NextDouble() * 100 - 50 ;
+        double result = SubtractionOfNumbers(number1, number2);
+        
+        Console.WriteLine(" Вариант 6 (Реализовать функцию вычисления разности двух вещественных чисел)");
+        if (number2 >= 0)
+        {
+            Console.WriteLine($"{number1} - {number2} = {result}");
+        }
+        else
+        {
+            Console.WriteLine($"{number1} - ({number2}) = {result}");
+        }
+    }
+    
+}
